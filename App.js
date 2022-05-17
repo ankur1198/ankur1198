72311c5ca9470ec9091910e073a0173336367c66
Ankur Mahanta <87887523+ankur1198@users.noreply.github.com>
2022-05-17 21:01:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import profile from "./a.png";
+import email from "./email.jpg";
+import pass from "./pass.jpg";
+function LoginUi() {
+  return (
+    <div className="main">
+     <div className="sub-main">
+       <div>
+         <div className="imgs">
+           <div className="container-image">
+             <img src={profile} alt="profile" className="profile"/>
+
+           </div>
+
+
+         </div>
+         <div>
+           <h2>Login to your account</h2>
+           <div>
+             <img src={email} alt="email" className="email"/>
+             <input type="text" placeholder="user name/ email" className="name"/>
+           </div>
+           <div className="second-input">
+             <img src={pass} alt="pass" className="email"/>
+             <input type="password" placeholder="password" className="name"/>
+           </div>
+          <div className="login-button">
+          <button>Log in</button>
+          </div>
+           
+            <p className="link">
+              <a href="#">Forgot password ?</a> <br></br>
+              <br></br>
+              New user?<a href="#">Sign up</a>
+            </p>
+           
+ 
+         </div>
+       </div>
+       
+
+     </div>
+    </div>
+  );
+}
+
+export default LoginUi;

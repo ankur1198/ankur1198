3a1238371624e4036bc6e6a20d06552e503f6a27
Ankur Mahanta <87887523+ankur1198@users.noreply.github.com>
2022-05-17 21:03:14 +0530

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/a.png 	new file:   src/email.jpg 	new file:   src/pass.jpg     new file:   src/pass1.png

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import profile from "./a.png";
+import email from "./email.jpg";
+import pass from "./pass.jpg";
+function LoginUi() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+     <div className="sub-main">
+       <div>
+         <div className="imgs">
+           <div className="container-image">
+             <img src={profile} alt="profile" className="profile"/>
+
+           </div>
+
+
+         </div>
+         <div>
+           <h2>Login to your account</h2>
+           <div>
+             <img src={email} alt="email" className="email"/>
+             <input type="text" placeholder="user name/ email" className="name"/>
+           </div>
+           <div className="second-input">
+             <img src={pass} alt="pass" className="email"/>
+             <input type="password" placeholder="password" className="name"/>
+           </div>
+          <div className="login-button">
+          <button>Log in</button>
+          </div>
+           
+            <p className="link">
+              <a href="#">Forgot password ?</a> <br></br>
+              <br></br>
+              New user?<a href="#">Sign up</a>
+            </p>
+           
+ 
+         </div>
+       </div>
+       
+
+     </div>
     </div>
   );
 }
 
-export default App;
+export default LoginUi;
